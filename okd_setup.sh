445b0ccf9beb6e01ff7f5f13a9bc0e65845c99bd
Robert Ongom <ongomr@a223460.local>
2018-11-16 21:49:34 +0200

    adding okd_setup.sh script that installs a simple okd standalone cluster

---
+++ okd_setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Sets up okd simple all-in-one localhost Installation
+# This script has only been tested on centos 7.5
+
+# Sources
+# github https://github.com/openshift/openshift-ansible
+
+# Fixes
+# https://github.com/openshift/openshift-ansible/issues/7794
+
+# Run the below manaully
+# Setting Repos and Updating OS
+sudo yum -y update
+sudo yum -y install wget git
+sudo wget https://storage.googleapis.com/origin-ci-test/releases/openshift/origin/master/origin.repo -O /etc/yum.repos.d/origin.repo
+sudo yum -y install epel-release
+sudo yum-config-manager --enable epel
+sudo yum-config-manager --enable origin-repo
+sudo yum -y update
+
+# Installing System Packages
+sudo yum install -y NetworkManager
+sudo systemctl start NetworkManager
+sudo systemctl enable NetworkManager
+
+# Installing Docker
+sudo yum install -y docker net-tools
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Installing Ansible and Openshift-Ansible Dependencies
+sudo yum -y install pyOpenSSL python-lxml python-jinja2
+sudo yum -y install http://cbs.centos.org/kojifiles/packages/ansible/2.6.5/1.el7/noarch/ansible-2.6.5-1.el7.noarch.rpm
+
+# Setting Public IP and Hostname
+#sudo echo 'okdsrv' | sudo tee --append /etc/hostname
+sudo echo '3.8.112.203   okdsrv' | sudo tee --append /etc/hosts
+sudo hostnamectl set-hostname okdsrv
+
+# Reboot the server and run the below manually
+# Cloning Openshift-Ansible Repo and Installing a Simple all-in-one okd cluster
+git clone https://github.com/openshift/openshift-ansible
+cd openshift-ansible
+sudo ansible-playbook -i inventory/hosts.localhost playbooks/prerequisites.yml
+sudo ansible-playbook -i inventory/hosts.localhost playbooks/deploy_cluster.yml
